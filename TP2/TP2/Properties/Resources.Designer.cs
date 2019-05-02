a7d6f815b29e257ecdbf8dc76b9a7cfb251f53f7
nphalakhone <nick.phalakhone@gmail.com>
2019-05-01 21:03:24 -0400

    added a bg to ressources

--- TP2/TP2/Properties/Resources.Designer.cs
+++ TP2/TP2/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace TP2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bgcolor {
+            get {
+                object obj = ResourceManager.GetObject("bgcolor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap calendar {
             get {
                 object obj = ResourceManager.GetObject("calendar", resourceCulture);
