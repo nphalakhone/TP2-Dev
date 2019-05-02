6a231e886b86cb50e1999ca20a8b085f7ef479e2
nphalakhone <nick.phalakhone@gmail.com>
2019-05-02 07:55:56 -0400

    added buttons for buying animals

--- TP2/TP2/FenetrePrincipale.Designer.cs
+++ TP2/TP2/FenetrePrincipale.Designer.cs
@@ -42,6 +42,10 @@
             this.TimerPrincipal = new System.Windows.Forms.Timer(this.components);
             this.LblDollarSign = new System.Windows.Forms.Label();
             this.TimerDate = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PicArgent)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicDate)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicAnimal)).BeginInit();
@@ -167,11 +171,51 @@
             this.TimerDate.Interval = 820;
             this.TimerDate.Tick += new System.EventHandler(this.TimerDate_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1404, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1444, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1429, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1480, 465);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // FenetrePrincipale
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1584, 938);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.LblDollarSign);
             this.Controls.Add(this.PicDechets);
             this.Controls.Add(this.PicAnimal);
@@ -211,6 +255,10 @@
         private System.Windows.Forms.Timer TimerPrincipal;
         private System.Windows.Forms.Label LblDollarSign;
         private System.Windows.Forms.Timer TimerDate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
